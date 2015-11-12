cdbc3626f48a9de4861707b16ac3fd39aacaec5a
Matthew Riley <Matt@Matthews-MacBook-Pro.local>
2015-11-12 18:48:19 -0500

    added code to add an Event to the calendar on click

--- scheduler/calendar.js
+++ scheduler/calendar.js
@@ -1,5 +1,23 @@
 $(function () {
+    var events = [
+    {
+        title: 'Test Event',
+        date: '2015-11-12'
+    }
+    ];
+
+    $('#add').click(function() {
+        ev = {
+            title: 'asda',
+            date: '2015-11-13',
+            stick: true
+        };
+        $('#calendar').fullCalendar('renderEvent', ev);
+    });
+
 	$('#calendar').fullCalendar({
-		
+        editable: true,
+        eventLimit: true,
+        events: events
 	});
 });
